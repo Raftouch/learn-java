f5ef49c2c8ee215ec061b1b3c50ba3cbe12bf2bd
raftouch <rafanelkin@gmail.com>
2025-11-12 17:01:13 +0100

    :white_check_mark: lesson2

---
+++ lessons/Lesson2.java
@@ -0,0 +1,19 @@
+package lessons;
+
+public class Lesson2 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 2;
+
+        // x += y;
+        // x -= y;
+        x *= y;
+        // x /= y;
+        // x %= y;
+
+        // x++;
+        // x--;
+
+        System.out.println(x);
+    }
+}
