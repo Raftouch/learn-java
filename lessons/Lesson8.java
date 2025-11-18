751bf2b804118b9b32f46c782e5dec66010b01dc
raftouch <rafanelkin@gmail.com>
2025-11-18 14:57:12 +0100

    :white_check_mark: lesson8

---
+++ lessons/Lesson8.java
@@ -0,0 +1,21 @@
+package lessons;
+
+public class Lesson8 {
+    public static void main(String[] args) {
+
+        String name = "Rafa";
+        // int length = name.length();
+        // char letter = name.charAt(0);
+        // int index = name.indexOf("a");
+        // lastIndexOf()
+        // toUpperCase() ..
+        // trim()
+        name = name.replace("a", "u");
+        // isEmpty()
+        // contains()
+        // equals()
+        // equalsIgnoreCase()
+
+        System.out.println(name);
+    }
+}
